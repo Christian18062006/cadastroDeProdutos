5107dd4f0aa5b34a10c5f378afbffb823c21d908
Carlos.Christian <carlos.programation@hotmail.comgit config --global user.email carlos.programation@hotmail.com>
2024-03-11 15:45:13 -0300

    Adicionado o método de verificar porcentagem

--- Produto.cs
+++ Produto.cs
@@ -9,5 +9,24 @@ namespace AdicionarProdutos
         public string nomeProduto = string.Empty;
         public double valorProduto;
         public int quantidadeProduto;
+        public int verificaPorcentagem()
+        {
+            if (quantidadeProduto<=10)
+            {
+                return (0);
+            }
+            else if(quantidadeProduto>=11 && quantidadeProduto<=20)
+            {
+                return (10);
+            }
+            else if(quantidadeProduto>=21 && quantidadeProduto<=50)
+            {
+                return (20);
+            }
+            else
+            {
+                return (25);
+            }
+        }
     }
 }
