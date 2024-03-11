c44420aeb7227ac3e9259e55617c90ba2966974d
Carlos.Christian <carlos.programation@hotmail.comgit config --global user.email carlos.programation@hotmail.com>
2024-03-11 15:36:13 -0300

    Criada a função para calcular o valor total do produto

--- Program.cs
+++ Program.cs
@@ -6,15 +6,15 @@ namespace AdicionarProdutos
     {
         public static int verificaPorcentagem(int quantidadeProduto)
         {
-            if(quantidadeProduto<=10)
+            if (quantidadeProduto <= 10)
             {
                 return (0);
             }
-            else if(quantidadeProduto>=11 && quantidadeProduto<=20)
+            else if (quantidadeProduto >= 11 && quantidadeProduto <= 20)
             {
                 return (10);
             }
-            else if(quantidadeProduto>=21 && quantidadeProduto<=50)
+            else if (quantidadeProduto >= 21 && quantidadeProduto <= 50)
             {
                 return (20);
             }
@@ -24,19 +24,26 @@ namespace AdicionarProdutos
             }
 
         }
-static void Main(string[] args)
+
+        public static double calculaValorTotal(double precoProduto, int valorPorcentagem)
         {
-            string nomeProduto = string.Empty;
-            double valorProduto;
-            int quantidadeProduto;
-            Console.WriteLine("Cadastro de produto.");
-            Console.WriteLine("Digite o nome do produto.");
-            nomeProduto = Console.ReadLine();
-            Console.WriteLine("Digite o valor do produto.");
-            valorProduto = double.Parse(Console.ReadLine());
-            Console.WriteLine("Qual é a quantidade que você deseja?");
-            quantidadeProduto = int.Parse(Console.ReadLine());
-            
+            double disconto = (precoProduto * valorPorcentagem) / 100;
+            double valorFinal = disconto - precoProduto;
+            return (valorFinal);
+        }
+            static void Main(string[] args)
+            {
+                string nomeProduto = string.Empty;
+                double valorProduto;
+                int quantidadeProduto;
+                Console.WriteLine("Cadastro de produto.");
+                Console.WriteLine("Digite o nome do produto.");
+                nomeProduto = Console.ReadLine();
+                Console.WriteLine("Digite o valor do produto.");
+                valorProduto = double.Parse(Console.ReadLine());
+                Console.WriteLine("Qual é a quantidade que você deseja?");
+                quantidadeProduto = int.Parse(Console.ReadLine());
+
+            }
         }
     }
-}
